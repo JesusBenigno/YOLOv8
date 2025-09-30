d3df5d2e2fbee090563fd757d675e9ac78d3f8d7
JesusBenigno <158539334+JesusBenigno@users.noreply.github.com>
2025-09-29 20:27:10 -0600

    Update augment.py for 3rd Albumentations
    Signed-off-by: JesusBenigno <158539334+JesusBenigno@users.noreply.github.com>

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -1954,10 +1954,8 @@ class Albumentations:
             # Transforms
             T = [
                 A.Blur(blur_limit=(2,5),p=0.3),
-                A.CLAHE(clip_limit=4.0, tile_grid_size=(8, 8), p=0.2),
-                A.RandomBrightnessContrast(brightness_limit=(-0.2, 0.2),contrast_limit=(-0.2, 0.2),p=0.2),
-                A.HueSaturationValue(hue_shift_limit=(-10,10), sat_shift_limit=(-20,20), val_shift_limit=(0,0), p=0.3),
-                A.Sharpen(alpha=(0.2, 0.5), lightness=(0.5, 1.0), method='kernel', p=0.1)
+                A.RandomBrightnessContrast(brightness_limit=(-0.2, 0.2),contrast_limit=(-0.2, 0.2),p=0.3),
+                A.Sharpen(alpha=(0.2, 0.5), lightness=(0.5, 1.0), method='kernel', p=0.3)
             ]
 
             # Compose transforms
